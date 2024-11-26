c70e6bc1897470e49012272affdeeef7dfa53cc8
André Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2024-11-26 08:32:24 +0200

    initialize Next.js project with TypeScript, ESLint, and basic layout structure

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { Providers } from './providers'
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Treasury Guild Dapp",
+  description: "Community Treasury Management",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
